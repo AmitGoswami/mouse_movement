9fce359622c7690d5f6f1313e4ee8bfc204772fe
AmitGoswami <amit.gosu@gmail.com>
2020-10-26 10:18:53 -0300

    Update mouse_click.bat

--- mouse_click.bat
+++ mouse_click.bat
@@ -1,17 +1,39 @@
 @echo off
-cls
-set time_out=%1
-set distance=%2
-IF [%time_out%] EQU [] (
-	echo Value Missing using default
-	set time_out=10
-)
-IF [%distance%] EQU [] (
-	echo distance Missing using default
-	set distance=1
-)
-:move_mouse
-echo moving mouse by %distance%x%distance%
-mouse.exe moveBy %distance%x%distance%
-timeout /t %time_out%
-goto move_mouse
+:: time in seconds
+set timeToWait=60
+
+:: time in 24 hrs format
+set activeTime=9
+set sleepingTime=22
+
+:startMouseMovement
+  goto checkTime
+
+
+:checkTime
+  echo checking time
+  echo current time is %time%
+  :: if time is less than 9 AM, please wait and dont move the mouse
+  if %time:~0,2% lss %activeTime% (
+    goto wait
+  ) 
+  :: if time is less than 19(7 PM), please wait and dont move the mouse
+  if %time:~0,2% geq %sleepingTime% (
+    goto wait
+  )
+  :: if time if greater than 9 AM, move the mouse so that monitor doesnt sleep
+  if %time:~0,2% geq %activeTime% (
+    goto moveMouse
+	goto wait
+  )
+  
+
+:moveMouse
+  echo 'moving mouse'
+  mouse.exe moveBy 1x1
+  goto wait
+  
+
+:wait
+  timeout /t %timeToWait%
+  goto startMouseMovement
